cfd0dd8b8eb3012a3f786ac61e7da318f20a0fe6
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2023-01-04 13:17:17 -0500

    Update radar_chart2.py

--- radar_chart2.py
+++ radar_chart2.py
@@ -19,11 +19,11 @@ import matplotlib.patheffects as path_effects
 
 ##################################################################################################################################
 
-font_path = 'Resources/fonts/keymer-bold.otf'  # Your font path goes here
+font_path = 'keymer-bold.otf'  # Your font path goes here
 font_manager.fontManager.addfont(font_path)
 prop = font_manager.FontProperties(fname=font_path)
 
-font_path = 'Resources/fonts/BasierCircle-Italic.ttf'  # Your font path goes here
+font_path = 'BasierCircle-Italic.ttf'  # Your font path goes here
 font_manager.fontManager.addfont(font_path)
 prop1 = font_manager.FontProperties(fname=font_path)
 
